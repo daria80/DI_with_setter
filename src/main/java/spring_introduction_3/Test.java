a45e7cd6bd8443156eabb9c5169e3d1cdb34bbcf
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:09:03 +0300

    add changes

--- src/main/java/spring_introduction_3/Test.java
+++ src/main/java/spring_introduction_3/Test.java
@@ -6,12 +6,15 @@ public class Test {
 
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
-
-        Library library = context.getBean("library", Library.class);
-
+        Book book = new Science();
+        Library library = new Library();
+        library.setBook(book);
         library.giveNeedBook();
 
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
+        Library library1 = context.getBean("library", Library.class);
+        library1.giveNeedBook();
         context.close();
     }
 }
