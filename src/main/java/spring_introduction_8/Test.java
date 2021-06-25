b608d49e54fc2dc7d4f48520d535eaf5e4b44aa8
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:17:17 +0300

    add changes

--- src/main/java/spring_introduction_8/Test.java
+++ src/main/java/spring_introduction_8/Test.java
@@ -5,6 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
 
+        Vegetable vegetable = new Beet();
+        GardenBed gardenBed = new GardenBed();
+        gardenBed.setVegetable(vegetable);
+        gardenBed.sowedInTheGarden();
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext8.xml");
         GardenBed gardenBed1 = context.getBean("gardenBed", GardenBed.class);
         gardenBed1.sowedInTheGarden();
