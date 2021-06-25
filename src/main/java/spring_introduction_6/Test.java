7526cb146df052317c859f7b3d923f110b5e296c
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:14:39 +0300

    add changes

--- src/main/java/spring_introduction_6/Test.java
+++ src/main/java/spring_introduction_6/Test.java
@@ -5,10 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext6.xml");
-        Application application = context.getBean("application", Application.class);
-
+        Message message = new Text();
+        Application application = new Application();
+        application.setMessage(message);
         application.sendMessage();
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext6.xml");
+        Application application1 = context.getBean("application", Application.class);
+        application1.sendMessage();
 
         context.close();
     }
