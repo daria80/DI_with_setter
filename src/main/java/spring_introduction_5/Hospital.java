60d440ca206a51f930f570225ae7e18b6a2b12c5
smirnova <dddsssmmm555@mail.ru>
2021-06-25 15:28:41 +0300

    add changes

--- src/main/java/spring_introduction_5/Hospital.java
+++ src/main/java/spring_introduction_5/Hospital.java
@@ -4,7 +4,12 @@ public class Hospital {
 
     public Child child;
 
-    public Hospital(Child child) {
+    public Hospital() {
+        System.out.println("Hospital is created");
+    }
+
+    public void setChild(Child child) {
+        System.out.println("Hospital class: set child");
         this.child = child;
     }
 
