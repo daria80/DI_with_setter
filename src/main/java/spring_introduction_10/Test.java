8b1f9681f2b37b1e5e321b28e700098d66dd6f58
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:21:05 +0300

    add changes

--- src/main/java/spring_introduction_10/Test.java
+++ src/main/java/spring_introduction_10/Test.java
@@ -6,6 +6,11 @@ public class Test {
 
     public static void main(String[] args) {
 
+        Banknote banknote = new Ruble();
+        ATM atm = new ATM();
+        atm.setBanknote(banknote);
+        atm.cash();
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext10.xml");
         ATM atm1 = context.getBean("atm", ATM.class);
         atm1.cash();
