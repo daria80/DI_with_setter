560412704b17511bbab5a3ec504628952beb98ed
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:15:50 +0300

    add changes

--- src/main/java/spring_introduction_7/Test.java
+++ src/main/java/spring_introduction_7/Test.java
@@ -6,9 +6,14 @@ public class Test {
 
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext7.xml");
-        Apartment apartment = context.getBean("apartment", Apartment.class);
+        Room room = new Bath();
+        Apartment apartment = new Apartment();
+        apartment.setRoom(room);
         apartment.isHad();
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext7.xml");
+        Apartment apartment1 = context.getBean("apartment", Apartment.class);
+        apartment1.isHad();
 
         context.close();
     }
