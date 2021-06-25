ab2ac8ff74da9a7e3b7c403903623b8930051b23
smirnova <dddsssmmm555@mail.ru>
2021-06-25 16:19:04 +0300

    add changes

--- src/main/java/spring_introduction_9/Test.java
+++ src/main/java/spring_introduction_9/Test.java
@@ -5,6 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test {
     public static void main(String[] args) {
 
+        Language language = new English();
+        Translator translator = new Translator();
+        translator.setLanguage(language);
+        translator.translate();
+
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext9.xml");
         Translator translator1 = context.getBean("translator",Translator.class);
         translator1.translate();
